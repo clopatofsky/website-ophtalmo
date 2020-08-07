1e0597ea0099b2936469a92a049fdde70979d962
Carlos <carlos_omana@mckinsey.com>
2020-08-06 22:23:28 -0500

    Adding mx records

--- terraform/serverless-website/dns.tf
+++ terraform/serverless-website/dns.tf
@@ -4,10 +4,11 @@ resource "aws_route53_zone" "zone" {
 
 // Workaround to get lower NS ttl for root domain
 resource "aws_route53_record" "ns" {
+  allow_overwrite = true
   zone_id = aws_route53_zone.zone.zone_id
   name    = var.root_domain
   type    = "NS"
-  ttl     = "86400"
+  ttl     = "172800"
   records = [
     aws_route53_zone.zone.name_servers[0],
     aws_route53_zone.zone.name_servers[1],
@@ -62,4 +63,20 @@ resource "aws_route53_record" "non-www-ipv6" {
     zone_id                = aws_cloudfront_distribution.non-www_distribution.hosted_zone_id
     evaluate_target_health = false
   }
+}
+
+resource "aws_route53_record" "gsuite_mx" {
+  zone_id = aws_route53_zone.zone.zone_id
+  name    = ""
+  type    = "MX"
+
+  records = [
+    "1 ASPMX.L.GOOGLE.COM",
+    "5 ALT1.ASPMX.L.GOOGLE.COM",
+    "5 ALT2.ASPMX.L.GOOGLE.COM",
+    "10 ALT3.ASPMX.L.GOOGLE.COM",
+    "10 ALT4.ASPMX.L.GOOGLE.COM",
+  ]
+
+  ttl = "3600"
 }
